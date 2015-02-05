574830c089b5982fbc7e8e6093a499108007c7ab
pathofneo <pathofneo@live.cn>
2015-02-05 17:01:07 +0800

    towerAOI:add many functions

--- towerAOI.cpp
+++ towerAOI.cpp
@@ -1,6 +1,181 @@
+#include "towerAOI.h"
 
-
-bool TowerAOI::isInRect(&Position pos,&Position start, &Position end)
+bool TowerAOI::isInRect(Position &pos,Position &start, Position &end)
 {
 	return ((pos.X >= start.X) && (pos.X <= end.X) && (pos.Y >= start.Y) && (pos.Y <= end.Y));
+}
+
+map_int_int_int TowerAOI::GetIdsByRange(Position pos, int r, int* types, int length)
+{
+	Position start;
+	Position end;
+	map_int_int_int result;
+	if (checkPos(pos) || (r < 0) || (r > rangeLimit)) 
+	{
+		return result;
+	}
+
+	Position p = transPos(pos);
+/*	if (p == NULL) 
+	{
+		cout<<"p value is nil.\n"
+	}*/
+	//Position* pt = 
+	getPosLimit(p, r, max,&start,&end);
+	//Position start = *pt; 
+	//Position end = *(pt+1);
+	
+	for (int i = start.X; i <= end.X; i++) 
+	{
+		for (int j = start.Y; j <= end.Y; j++) 
+		{
+			result = addMapByTypes(result, towers[i][j].GetIdsByTypes(types,length), types)
+		}
+	}
+	return result;
+}
+
+void TowerAOI::getPosLimit(Position pos, int r , Position max, Position* start , Position* end)// (start *Position, end *Position)//返回一个数组指针
+{
+
+	if (pos.X-r < 0) {
+		start->X = 0;
+		end->X = 2 * r;
+	} else if (pos.X+r > max.X) {
+		end->X = max.X;
+		start->X = max.X - 2*r;
+	} else {
+		start->X = pos.X - r;
+		end->X = pos.X + r;
+	}
+
+	if (pos.Y-r < 0) {
+		start->Y = 0;
+		end->Y = 2 * r;
+	} else if (pos.Y+r > max.Y) {
+		end->Y = max.Y;
+		start->Y = max.Y - 2*r;
+	} else {
+		start->Y = pos.Y - r;
+		end->Y = pos.Y + r;
+	}
+
+	if (start->X < 0) {
+		start->X = 0;
+	}
+	if (end->X > max.X) {
+		end->X = max.X;
+	}
+
+	if (start->Y < 0) {
+		start->Y = 0;
+	}
+	if (end->Y > max.Y) {
+		end->Y = max.Y;
+	}
+	
+}
+
+map_int_int_l TowerAOI::GetIdsByPos(Position pos, int r )
+{
+	map_int_int_l result;
+	Position start,end,p;
+	
+	result.length=0;
+	if (checkPos(pos) || (r < 0)) 
+	{
+		return result;
+	}
+
+	if (r > 5) 
+	{
+		r = 5;
+	}
+
+	p = transPos(pos);
+	getPosLimit(p, r, max,&start,&end);
+	for (int i = start.X; i <= end.X; i++) 
+	{
+		for (int j = start.Y; j <= end.Y; j++) 
+		{
+			addMap(result, towers[i][j].GetIds(),&result.content,&result.length);
+		}
+	}
+	return result;
+}
+
+bool TowerAOI::checkPos(Position pos )
+{
+
+	if ((pos.X < 0) || (pos.Y < 0) || (pos.X >= M.Width) || (pos.Y >= M.Height)) {
+		return false;
+	}
+	return true;
+}
+
+bool TowerAOI::AddObject(Object obj, Position pos)
+{
+	Position p;
+	if (checkPos(pos)) {
+		p = transPos(pos);
+		towers[p.X][p.Y].Add(&obj);
+		// emit('add', {id: obj.id, type:obj.type, watchers:this.towers[p.x][p.y].watchers});
+		return true;
+	}
+	return false;
+}
+
+
+bool TowerAOI::RemoveObject(Object obj, Position pos)
+{
+	if (checkPos(pos)) {
+		p = transPos(pos);
+		towers[p.X][p.Y].Remove(obj);
+		// this.emit('remove', {id: obj.id, type:obj.type, watchers:this.towers[p.x][p.y].watchers});
+		return true;
+	}
+	return false;
+}
+
+Position TowerAOI::transPos(Position pos)
+{
+	Position p;
+	p.X = int(floor(float(pos.X) / float(T.Width)));
+	p.Y = int(floor(float(pos.Y) / float(T.Width)));
+	return p;
+}
+
+map_int_int_int TowerAOI::GetWatchers(Position pos, int* types,int length)
+{
+	map_int_int_int result;
+	Position p;
+	
+	if (checkPos(pos)) {
+		p = transPos(pos);
+		return towers[p.X][p.Y].GetWatchers(types,length);
+	}
+	return result;
+}
+
+void TowerAOI::AddWatcher(Object watcher, Position pos, int r )
+{
+	Position start,end,p;
+	if (r < 0) {
+		return;
+	}
+
+	if (r > 5) {
+		r = 5;
+	}
+
+	p = transPos(pos);
+	getPosLimit(p, r, max,&start,&end);
+
+	for (int i = start.X; i <= end.X; i++) 
+	{
+		for (int j = start.Y; j <= end.Y; j++) 
+		{
+			towers[i][j].AddWatcher(&watcher);
+		}
+	}
 }
--- towerAOI.h
+++ towerAOI.h
@@ -1,4 +1,5 @@
-
+#include "tower.cpp"
+#include <math.h>
 
 typedef struct Rectangle_  {
 	int Width;
@@ -9,12 +10,27 @@ typedef struct Position_ {
 	int X; 
 	int Y;
 }Position;
+typedef map<int,map<int,Tower> > map_int_int_Tower; 
 
-/*typedef  struct MapConfig_{
-	*Rectangle
+typedef struct map_int_int_length_ {
+	map_int_int content; 
+	int length;
+}map_int_int_l;
+
+typedef  struct MapConfig_{
+	int Width;
+	int Height;
 	int ID;
 }MapConfig;
 
+
+typedef  struct TowerConfig_{
+	int Width;
+	int Height;
+	int ID;
+}TowerConfig;
+
+/*
 type TowerConfig struct {
 	*Rectangle
 }
@@ -29,39 +45,50 @@ class TowerAOI
 {
 private:
 	//*Config
+	MapConfig M;
 	int width;      
 	int height;     
-	Tower[][] towers;     
+	map_int_int_Tower towers;     
 	int rangeLimit; 
 	//*
 	Position max;   
 
 public:
-	bool isInRect(&Position pos,&Position start, &Position end);
+	bool isInRect(Position &pos,Position &start, Position &end);
 	//int[] addMap(int[] result, m map[int][]) 
-	
-}
+	map_int_int_int GetIdsByRange(Position pos, int r, int* types, int length);
+	void getPosLimit(Position pos, int r, Position max, Position* start , Position* end);// (start *Position, end *Position)//返回一个数组指针
+	map_int_int_l GetIdsByPos(Position pos, int r );
+	bool checkPos( Position pos);
+	bool AddObject(Object obj, Position pos);
+	bool RemoveObject(Object obj, Position pos);
+	Position transPos(Position pos); 	
+	map_int_int_int GetWatchers(Position pos, int* types,int length);
+	void AddWatcher(Object watcher, Position pos, int r );
+};
 //func isInRect(pos *Position, start *Position, end *Position) bool 
 
 
 
 func (t *TowerAOI) Init(config *Config) 
-func (t *TowerAOI) GetIdsByRange(pos *Position, r int, types []int) map[int]map[int64]int64
-func (t *TowerAOI) GetIdsByPos(pos *Position, r int) []int64 {
-func (t *TowerAOI) AddObject(obj *Object, pos *Position) bool {
-func (t *TowerAOI) RemoveObject(obj *Object, pos *Position) bool
+//func (t *TowerAOI) GetIdsByRange(pos *Position, r int, types []int) map[int]map[int64]int64
+//func (t *TowerAOI) GetIdsByPos(pos *Position, r int) []int64 { 
+//func (t *TowerAOI) AddObject(obj *Object, pos *Position) bool {
+//func (t *TowerAOI) RemoveObject(obj *Object, pos *Position) bool
 func (t *TowerAOI) UpdateObject(obj *Object, oldPos *Position, newPos *Position) bool
-func (t *TowerAOI) checkPos(pos *Position) bool
-func (t *TowerAOI) transPos(pos *Position) *Position
-func (t *TowerAOI) AddWatcher(watcher *Object, pos *Position, r int)
+//func (t *TowerAOI) checkPos(pos *Position) bool
+//func (t *TowerAOI) transPos(pos *Position) *Position
+//func (t *TowerAOI) AddWatcher(watcher *Object, pos *Position, r int)
 func (t *TowerAOI) RemoveWatcher(watcher *Object, pos *Position, r int) 
 func (t *TowerAOI) UpdateWatcher(watcher *Object, oldPos *Position, newPos *Position, oldRange int, newRange int) bool 
 func (t *TowerAOI) getChangedTowers(p1 *Position, p2 *Position, oldRange int, newRange int, towers [][]*Tower, max *Position) (removeTowers []*Tower, addTowers []*Tower, unChangeTowers []*Tower) 
-func (t *TowerAOI) getPosLimit(pos *Position, r int, max *Position) (start *Position, end *Position)
+//func (t *TowerAOI) getPosLimit(pos *Position, r int, max *Position) (start *Position, end *Position)//返回一个数组指针
 
-func (t *TowerAOI) GetWatchers(pos *Position, types []int) map[int]map[int64]int64 
+//func (t *TowerAOI) GetWatchers(pos *Position, types []int) map[int]map[int64]int64 
 
 func (t *TowerAOI) addMapByTypes(result map[int]map[int64]int64, m map[int]map[int64]int64, types []int) map[int]map[int64]int64
 //
 func addMap(result []int64, m map[int64]int64) []int64
-func CreateTowerAOI(config *Config) *TowerAOI
+func CreateTowerAOI(config *Config) *TowerAOI)
+
+*/
